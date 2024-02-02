4d8735fc84b32af0e41f5d8c2975835a1d786d1c
Reviakin Nikolai <n.reviakin@norsi-trans.ru>
2024-02-02 14:10:24 +0300

    fix: typo corrected

--- src/Components/Screens/ScreenCompletion/Completion.tsx
+++ src/Components/Screens/ScreenCompletion/Completion.tsx
@@ -3,7 +3,7 @@ import style from "../ScreenBenefit/ScreenBenefit.module.scss";
 export const ScreenCompletion = () => {
   return (
     <div className={style.screen}>
-      <h1>Что вы гарантированно приобретете по завершению цикла лекций?</h1>
+      <h1>Что вы гарантированно приобретете по завершении цикла лекций?</h1>
       <div>
         <span className={style.ellipse} /> Понимание основных принципов
         построения информационных технологий, что позволит посмотреть на мир IT
